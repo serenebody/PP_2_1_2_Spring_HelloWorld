d5cbc6c3ac545e94b4ae2bc9b6cdb6982005bc23
seren <nicitabegunov@yandex.ru>
2024-10-15 06:21:47 +0300

    Create bean Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean.equals(bean1));
+        System.out.println(beanCat1.equals(beanCat2));
     }
 }
